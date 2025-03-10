fe4f03cfd05678d7f78483198af5d1664cfc36e4
Francisco Saraiva <a49462@alunos.isel.pt>
2025-03-10 17:56:41 +0000

    Project structure initialization

---
+++ code/backend/settings.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    kotlin("jvm") version "2.0.10" apply false
+    kotlin("plugin.spring") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.3.3"  apply false
+    id("io.spring.dependency-management") version "1.1.6"  apply false
+}
+rootProject.name = "backend"
+include("domain")
+include("host")
+include("http")
+include("repository")
+include("repository-jdbi")
+include("services")
