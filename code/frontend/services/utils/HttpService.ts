369b2ebfdf7783123ab9ef45b68951963d0d6444
Francisco Saraiva <a49462@alunos.isel.pt>
2025-05-30 22:21:45 +0100

    feat: add Docker tasks for building images and managing containers

--- code/frontend/services/utils/HttpService.ts
+++ code/frontend/services/utils/HttpService.ts
@@ -1,6 +1,6 @@
 import { MEDIA_TYPE_PROBLEM, Problem } from "../media/Problem";
 
-const LOCAL_IP = "192.168.1.72";
+const LOCAL_IP = "localhost";
 const PORT = "8080";
 
 export const PREFIX_API = `http://${LOCAL_IP}:${PORT}/api`;
