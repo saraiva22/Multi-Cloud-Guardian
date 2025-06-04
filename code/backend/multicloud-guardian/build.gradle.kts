7b7cd2c4faf3fab52ba37e29edbef5f0108b3837
saraiva22 <a49462@alunos.isel.pt>
2025-06-04 16:07:44 +0100

    feat: update build configuration with Kotlin plugin and dependencies

--- code/backend/multicloud-guardian/build.gradle.kts
+++ code/backend/multicloud-guardian/build.gradle.kts
@@ -1,5 +1,21 @@
-task<Exec>("composeDown") {
-    commandLine("docker", "compose", "down")
+plugins {
+    kotlin("jvm") version "2.0.10"
 }
 
-extra["composeFileDir"] = layout.projectDirectory
+group = "org.example"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
